1f4f8797a409b95b0b3df8aa32400d13630e11e0
Mark Young <mark.francis.young@gmail.com>
2024-05-09 16:32:01 -0500

    load from specific dates for NDakota files

--- download_upload_google.py
+++ download_upload_google.py
@@ -4,6 +4,7 @@ import tempfile
 
 from google.cloud import storage
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from urllib.parse import urlparse
 
 
@@ -13,31 +14,57 @@ def download_file_from_uri(uri, file):
         file.write(chunk)
 
 
-def upload_file_to_gcs(file_path, file_date, bucket_name, file_name):
-    # Create a client object
-    client = storage.Client()
-
-    # Get the bucket object, create it if it doesn't exist
-    try:
-        bucket = client.get_bucket(bucket_name)
-    except Exception as e:
-        print(f"Bucket {bucket_name} does not exist, creating it...\n{e}")
-        bucket = client.create_bucket(bucket_name)
+def upload_file_to_gcs(file_path, file_date, bucket_name, file_name, dry_run=False):
 
     # Construct the path on Google Cloud Storage
     gcs_path = f'{file_date:%Y-%m-%d}/{file_name}'
 
-    # Upload the file to Google Cloud Storage
-    blob = bucket.blob(gcs_path)
-    blob.upload_from_filename(file_path)
+    if not dry_run:
+        # Create a client object
+        client = storage.Client()
+
+        # Get the bucket object, create it if it doesn't exist
+        try:
+            bucket = client.get_bucket(bucket_name)
+        except Exception as e:
+            print(f"Bucket {bucket_name} does not exist, creating it...\n{e}")
+            bucket = client.create_bucket(bucket_name)
+
+
+        # Upload the file to Google Cloud Storage
+        blob = bucket.blob(gcs_path)
+        blob.upload_from_filename(file_path)
 
     print(f'File uploaded to {gcs_path}')
 
 
-# Example usage:
-uri = 'https://www.dmr.nd.gov/oilgas/mpr/2015_05.xlsx'
-file_name = os.path.basename(urlparse(uri).path)
-with tempfile.NamedTemporaryFile() as file:
-    download_file_from_uri(uri, file)
-    file.flush()
-    upload_file_to_gcs(file.name, datetime(2015, 5, 1), 'north-dakota-daily-oil-gas-data-monthly', file_name)
+def generate_month_first_days(start_date, end_date):
+    # Initialize the list to store the dates
+    dates = []
+
+    # Iterate over the months
+    current_date = start_date
+    while current_date <= end_date:
+        # Add the first day of the month to the list
+        dates.append(current_date)
+        # Move to the next month
+        current_date += relativedelta(months=1)
+
+    return dates
+
+def store_north_dakota_files(start_date, end_date):
+    # first day of every month since and including 2015-05-01 until today
+    dates = generate_month_first_days(start_date, end_date)
+    for current_date in dates:
+        uri = f'https://www.dmr.nd.gov/oilgas/mpr/{current_date:%Y_%m}.xlsx'
+        file_name = os.path.basename(urlparse(uri).path)
+        with tempfile.NamedTemporaryFile() as file:
+            try:
+                download_file_from_uri(uri, file)
+                file.flush()
+            except Exception as e:
+                print(f'cannot load file for {uri}\n{e}')
+                next
+            upload_file_to_gcs(file.name, current_date, 'north-dakota-daily-oil-gas-data-monthly', file_name)
+
+store_north_dakota_files(start_date=datetime(2015, 5, 1), end_date=datetime.today())
